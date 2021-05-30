a0677a9908bfbc29e3295e93ef1ba1ad6d2d98bd
MD Shimul <mdshemul480@gmail.com>
2021-05-30 17:31:45 +0600

    adding cors to the backend

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,12 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
+
+// adding cors to the backend
+app.use(cors());
 
 // this 2 will help to get request body.. json as well as url params.
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -12,6 +16,7 @@ app.use(bodyParser.json());
 // redirecting routes to the payment routes
 app.use("/api/payment", require("./routes/payment-route"));
 
+// this will redirect request to the user route.
 app.use("/api/user", require("./routes/user-route"));
 
 // run the server after db connected.
