2cbd08f2232ee189144fe645aaa3d988a7e37d12
MD Shimul <mdshemul480@gmail.com>
2021-05-30 15:47:59 +0600

    adding username checker controller

---
+++ backend/controllers/alreadyExist.js
@@ -0,0 +1,34 @@
+const User = require("../models/user");
+
+const alreadyExist = async (req, res) => {
+  // getting username from the request
+  const { username } = req.body;
+
+  // checking if user already exist in the db or not.
+  let userExist;
+  try {
+    userExist = await User.findOne({ username });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      successful: false,
+      message: "something wrong with the server. ",
+    });
+  }
+  // if username found in db return true else false
+  if (userExist) {
+    return res.json({
+      successful: true,
+      message: "username already exist.",
+      found: true,
+    });
+  }
+
+  return res.json({
+    successful: true,
+    message: "username didn't exist.",
+    found: false,
+  });
+};
+
+module.exports = alreadyExist;
